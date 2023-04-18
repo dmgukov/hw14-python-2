77658c650cabb6ddb4958c75753d2deac376e904
dmytro-gukov <lazkoff@gmail.com>
2023-04-18 17:43:19 +0200

    Changed both .py file and notebook to return results, instead of just printing on the screen.

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ def check_addresses(address_list):
     # remove new line characters
     address_list = [x.strip() for x in address_list]
 
+    # list value to collect non-working addresses
+    results_list = []
+
     for address in address_list:
         try:
             response = requests.get(address, timeout=5)
             if response.status_code != 200:
-                print(f"{address} returned a status code of {response.status_code}")
+                results_list.append(address)
         except requests.exceptions.Timeout:
-            print(f"{address} timed out after 5 seconds")
+            results_list.append(f"{address} timed out after 5 seconds")
         except requests.exceptions.RequestException:
-            print(f"Failed to establish a connection with {address}")
+            results_list.append(f"Failed to establish a connection with {address}")
+    return results_list
 
 
 with open("address_list.txt") as f:
     address_list = f.readlines()
 
-check_addresses(address_list)
+results = check_addresses(address_list)
+print(results)
